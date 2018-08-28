3d4a69b0b5cecbec9501bed302d99d8ae62fb136
zehnpaard <zehnpaard@googlemail.com>
2018-08-28 11:53:48 +0900

    Bigger grammar

---
+++ ch02/grammar3.lisp
@@ -0,0 +1,54 @@
+(defparameter *simple-grammar*
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Noun))
+    (verb-phrase -> (Verb noun-phrase))
+    (Article -> the a)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked))
+  "A grammar for a trivial subset of English")
+
+(defvar *grammar* *simple-grammar*
+  "The grammar used by generate.  Initially, this is
+  *simple-grammar*, but we can switch to other grammars.")
+
+(defun rule-lhs (rule)
+  "The left-hand side of a rule"
+  (first rule))
+(defun rule-rhs (rule)
+  "The right-hand side of a rule"
+  (rest (rest rule)))
+(defun rewrites (category)
+  "Return a list of the possible rewrites for this category."
+  (rule-rhs (assoc category *grammar*)))
+
+(defun random-elt (choices)
+  "Choose an element from a list at random."
+  (elt choices (random (length choices))))
+
+(defun mappend (f xs)
+  (apply #'append (mapcar f xs)))
+
+(defun generate (phrase)
+  "Generate a random sentence or phrase"
+  (cond ((listp phrase)
+         (mappend #'generate phrase))
+        ((rewrites phrase)
+         (generate (random-elt (rewrites phrase))))
+        (t (list phrase))))
+
+(defparameter *bigger-grammar*
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Adj* Noun PP*) (Name) (Pronoun))
+    (verb-phrase -> (Verb noun-phrase PP*))
+    (PP* -> () (PP PP*))
+    (Adj* -> () (Adj Adj*))
+    (PP -> (Prep noun-phrase))
+    (Prep -> to in by with on)
+    (Adj -> big little blue green adiabatic)
+    (Article -> the a)
+    (Name -> Pat Kim Lee Terry Robin)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked)
+    (Pronoun -> he she it these those that)))
+
+(setf *grammar* *bigger-grammar*)
