9de3212b3de764c67976bda542492c82312f3e8e
zehnpaard <zehnpaard@googlemail.com>
2018-08-31 18:53:52 +0900

    Fix bugs in find-all

--- ch04/find-all.lisp
+++ ch04/find-all.lisp
@@ -1,4 +1,4 @@
-(defun find-all (item sequence 'rest keyword-args 'key (test #'eq) test-not 'allow-other-keys)
+(defun find-all (item sequence &rest keyword-args &key (test #'eql) test-not &allow-other-keys)
   "Find all those elements of sequence that match item,
   according to the keywords. Doesn't alter sequence."
   (if test-not
