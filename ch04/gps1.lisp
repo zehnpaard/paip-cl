f9009921a2603c95a3c7f35148b5e2ed2d5e50e7
zehnpaard <zehnpaard@googlemail.com>
2018-08-31 19:06:15 +0900

    Fix bugs in gps1

--- ch04/gps1.lisp
+++ ch04/gps1.lisp
@@ -21,7 +21,7 @@
   or if there is an appropriate op for it that is applicable"
   (or (member goal *state*)
       (some #'apply-op
-            (find-all goal *ops :test #'appropriate-p))))
+            (find-all goal *ops* :test #'appropriate-p))))
 
 (defun appropriate-p (goal op)
   "An op is appropriate to a goal if it is in its add list."
@@ -32,5 +32,5 @@
   (when (every #'achieve (op-preconds op))
     (print (list 'executing (op-action op)))
     (setf *state* (set-difference *state* (op-del-list op)))
-    (setf *state (union *state* (op-add-list op)))
+    (setf *state* (union *state* (op-add-list op)))
     t))
