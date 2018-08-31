ba9f7489f89f09288ba35de7bb6053a6fd45bab7
zehnpaard <zehnpaard@googlemail.com>
2018-08-31 18:32:34 +0900

    Implement find-all from chapter 3

---
+++ ch04/find-all.lisp
@@ -0,0 +1,8 @@
+(defun find-all (item sequence 'rest keyword-args 'key (test #'eq) test-not 'allow-other-keys)
+  "Find all those elements of sequence that match item,
+  according to the keywords. Doesn't alter sequence."
+  (if test-not
+    (apply #'remove item sequence
+           :test-not (complement test-not) keyword-args)
+    (apply #'remove item sequence
+           :test (complement test) keyword-args)))
