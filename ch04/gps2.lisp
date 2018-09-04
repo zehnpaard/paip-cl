0f2e9005c24c75af6649cb9d502c4fa21f05a225
zehnpaard <zehnpaard@googlemail.com>
2018-09-04 19:13:36 +0900

    Correct issues in gps2

--- ch04/gps2.lisp
+++ ch04/gps2.lisp
@@ -1,26 +1,5 @@
 (load "find-all.lisp")
-(load "test1.lisp")
-(load "debug.lisp")
-
-(defun executing-p (x)
-  "Is x of the form+ (executing ...)?"
-  (starts-with x 'executing))
-
-(defun starts-with (xs x)
-  "Is this a list whose first element is x?"
-  (and (consp xs) (eql (first xs) x)))
-
-(defun convert-op (op)
-  "Make op conform to the (EXECUTING op) convention"
-  (unless (some #'executing-p (op-add-list op))
-    (push (list 'executing (op-action op)) (op-add-list op)))
-  op)
-
-(defun op (action 'key preconds add-list del-list)
-  "Make a new operator that obeys the (EXECUTING op) convention"
-  (convert-op
-    (make-op :action action :preconds preconds
-             :add-list add-list :del-list del-list)))
+(load "dbg1.lisp")
 
 (defvar *ops* nil
   "A list of available operators")
@@ -28,19 +7,32 @@
 (defstruct op "An operation"
   (action nil) (preconds nil) (add-list nil) (del-list nil))
 
-(defun GPS (state goals &optional (*ops* *ops*))
-  "General Problem Solver: from state, achieve goals using *ops*"
-  (remove-if #'atom (achieve-all (cons '(start) state) goals nil)))
+(defun starts-with (xs x)
+  "Is this a list whose first element is x?"
+  (and (consp xs) (eql (first xs) x)))
 
-(defun achieve-all (state goals goal-stack)
-  "Achieve each goal, and make sure they still hold at the end"
-  (let ((current-state state))
-    (if (and (every #'(lambda (g)
-                        (setf current-state
-                              (achieve current-state g goal-stack)))
-                    goals)
-             (subsetp goals current-state :test #'equal))
-      current-state)))
+(defun executing-p (x)
+  "Is x of the form+ (executing ...)?"
+  (starts-with x 'executing))
+
+(defun convert-op (op)
+  "Make op conform to the (EXECUTING op) convention"
+  (unless (some #'executing-p (op-add-list op))
+    (push (list 'executing (op-action op)) (op-add-list op)))
+  op)
+
+(defun op (action &key preconds add-list del-list)
+  "Make a new operator that obeys the (EXECUTING op) convention"
+  (convert-op
+    (make-op :action action :preconds preconds
+             :add-list add-list :del-list del-list)))
+
+(defun member-equal (item xs)
+  (member item xs :test #'equal))
+
+(defun appropriate-p (goal op)
+  "An op is appropriate to a goal if it is in its add-list"
+  (member-equal goal (op-add-list op)))
 
 (defun achieve (state goal goal-stack)
   "A goal is achieved if it already holds,
@@ -51,8 +43,15 @@
         (t (some #'(lambda (op) (apply-op state goal op goal-stack))
                  (find-all goal *ops* :test #'appropriate-p)))))
 
-(defun member-equal (item xs)
-  (member item xs :test #'equal))
+(defun achieve-all (state goals goal-stack)
+  "Achieve each goal, and make sure they still hold at the end"
+  (let ((current-state state))
+    (if (and (every #'(lambda (g)
+                        (setf current-state
+                              (achieve current-state g goal-stack)))
+                    goals)
+             (subsetp goals current-state :test #'equal))
+      current-state)))
 
 (defun apply-op (state goal op goal-stack)
   "Return a new, transformed state if op is applicable"
@@ -67,9 +66,10 @@
                          state2)
               (op-add-list op)))))
 
-(defun appropriate-p (goal op)
-  "An op is appropriate to a goal if it is in its add-list"
-  (member-equal goal (op-add-list op)))
+(defun GPS (state goals &optional (*ops* *ops*))
+  "General Problem Solver: from state, achieve goals using *ops*"
+  (remove-if #'atom (achieve-all (cons '(start) state) goals nil)))
+
 
 (defun use (oplist)
   "Use oplist as the default list of operators"
